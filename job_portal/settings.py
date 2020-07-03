2006f082552e15cadf81c72add9d34e4faf26df1
Naik <Mujtabahussain.Naik@CenturyLink.com>
2020-07-03 22:16:06 +0530

    changed settings

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -15,6 +15,7 @@ import os
 import dj_database_url
 import psycopg2
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "job_portal.settings")
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
